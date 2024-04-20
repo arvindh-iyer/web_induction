d318559d947de862774d98eb726cb136c66257a7
Arvindh Iyer <2021ugcs013@nitjsr.ac.in>
2024-04-20 20:07:49 +0530

    h1

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,16 @@ var corsOptions = {
   credentials: true,
 };
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 db_connect();
 
